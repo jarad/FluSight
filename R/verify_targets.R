816880afeb2caf94af495c2703f6b5e178c8b349
Jarad Niemi <niemi@iastate.edu>
2016-10-06 12:10:55 -0500

    Add verify_targets functionality and test code
    Fixes #9

--- R/verify_targets.R
+++ R/verify_targets.R
@@ -11,5 +11,17 @@
 #' @examples
 #' verify_targets(minimal_entry)
 verify_targets <- function(entry) {
+  valid_targets <- unique(minimal_entry$target)
+  entry_targets <- unique(entry$target)
+  
+  missing_targets <- setdiff(valid_targets, entry_targets)
+  extra_targets   <- setdiff(entry_targets, valid_targets)
+
+  if (length(missing_targets)>0)
+    stop("Missing these targets: ", paste(missing_targets))
+
+  if (length(extra_targets)>0)
+    warning("These extra targets are ignored: ", paste(extra_targets))
+  
   return(invisible(TRUE))
 }
---
+++ tests/testthat/test-verify_targets.R
@@ -0,0 +1,21 @@
+context("verify_targets")
+
+test_that("Correct entries are successful.",{
+  expect_true(verify_targets(minimal_entry))
+  expect_true(verify_targets(full_entry   ))
+})
+
+test_that("Missing targets report errors.", {
+  valid_targets <- unique(minimal_entry$target)
+  
+  for (i in seq_along(valid_targets)) {
+    tmp_entry <- minimal_entry[minimal_entry$target != valid_targets[i],]
+    expect_error(verify_targets(tmp_entry))
+  }
+})
+
+test_that("Extra target reports warning.", {
+  tmp_entry <- minimal_entry
+  tmp_entry$target[1] <- "extra target"
+  expect_warning(verify_targets(tmp_entry))
+})
