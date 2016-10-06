54e5dcb16ec8d424654a714a05593ac528f1a94c
Jarad Niemi <niemi@iastate.edu>
2016-10-06 11:21:34 -0500

    Add verify_colnames

---
+++ R/verify_colnames.R
@@ -0,0 +1,24 @@
+#' Verify the column names of an entry
+#'
+#' Compares column names to a valid entry and provides an error if any names
+#' are missing and a warning if there are any extra names.
+#'
+#' @param entry An entry data.frame
+#' @return Invisibly returns true if successful
+#' @export
+#' @keywords internal
+verify_colnames <- function(entry) {
+  entry_names <- colnames(entry)
+  valid_names <- colnames(minimal_entry)
+
+  missing_names <- setdiff(valid_names, entry_names)
+  extra_names   <- setdiff(entry_names, valid_names)
+
+  if (length(missing_names)>0)
+    stop("Missing these columns: ", paste(missing_names))
+
+  if (length(extra_names)>0)
+    warning("These extra columns are ignored: ", paste(extra_names))
+
+  return(invisible(TRUE))
+}
---
+++ tests/testthat/test-verify_colnames.R
@@ -0,0 +1,19 @@
+context("verify_colnames")
+
+test_that("Correct entries are successful.",{
+  expect_true(verify_colnames(minimal_entry))
+  expect_true(verify_colnames(full_entry   ))
+})
+
+test_that("Missing columns report errors.", {
+  for (i in seq_along(colnames(minimal_entry))) {
+    tmp_entry <- minimal_entry[,-i]
+    expect_error(verify_colnames(tmp_entry))
+  }
+})
+
+test_that("Extra column report warnings.", {
+  tmp_entry <- minimal_entry
+  tmp_entry$extra_column = NA
+  expect_warning(verify_colnames(tmp_entry))
+})
