139c1f763430888ba9c74ba9221eda55ffdd8b67
Jarad Niemi <niemi@iastate.edu>
2016-10-06 15:04:31 -0500

    Add an implementation of score_entry without including additional bins

--- R/score_entry.R
+++ R/score_entry.R
@@ -1,14 +1,12 @@
 #' Scores an entry
 #'
-#' @param entry An entry data.frame
-#' @param truth A data.frame of true values
-#' @param percent_bin_width An integer (default 5) indicating the number of
-#' surrounding bins in each direction to sum for percent targets.
-#' @param week_bin_width An integer (default 1) indicating the number of
-#' surounding bins in each direction to sum for week targets.
+#' @param entry An entry data.frame with columns location, target,
+#' bin_start_incl, and value
+#' @param truth A data.frame with columns location, target, and bin_start_incl
 #' @import dplyr
 #' @return A data.frame of scores for each target
-score_entry <- function(entry, truth, percent_bin_width = 5,
-                        week_bin_width = 1) {
-
+score_entry <- function(entry, truth) {
+  entry %>%
+    filter(type == "Bin") %>%
+    right_join(truth, by=c("location","target","bin_start_incl"))
 }
