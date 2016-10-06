46659365e972464074ce4e509204d90b2b7deb15
Jarad Niemi <niemi@iastate.edu>
2016-10-06 12:15:09 -0500

    Add functionality to verify types and tests
    Fixes #11

--- R/verify_types.R
+++ R/verify_types.R
@@ -1,6 +1,6 @@
 #' Verify types are correct
 #'
-#' The necessary types depend on the target, so this will verify types are correct
+#' The necessary types depend on the type, so this will verify types are correct
 #' for all types
 #'
 #' @param entry An entry data.frame
@@ -11,5 +11,17 @@
 #' @examples
 #' verify_types(minimal_entry)
 verify_types <- function(entry) {
+  valid_types <- unique(minimal_entry$type)
+  entry_types <- unique(entry$type)
+  
+  missing_types <- setdiff(valid_types, entry_types)
+  extra_types   <- setdiff(entry_types, valid_types)
+
+  if (length(missing_types)>0)
+    stop("Missing these types: ", paste(missing_types))
+
+  if (length(extra_types)>0)
+    warning("These extra types are ignored: ", paste(extra_types))
+  
   return(invisible(TRUE))
 }
---
+++ tests/testthat/test-verify_types.R
@@ -0,0 +1,21 @@
+context("verify_types")
+
+test_that("Correct entries are successful.",{
+  expect_true(verify_types(minimal_entry))
+  expect_true(verify_types(full_entry   ))
+})
+
+test_that("Missing types report errors.", {
+  valid_types <- unique(minimal_entry$type)
+  
+  for (i in seq_along(valid_types)) {
+    tmp_entry <- minimal_entry[minimal_entry$type != valid_types[i],]
+    expect_error(verify_types(tmp_entry))
+  }
+})
+
+test_that("Extra type reports warning.", {
+  tmp_entry <- minimal_entry
+  tmp_entry$type[1] <- "extra type"
+  expect_warning(verify_types(tmp_entry))
+})
