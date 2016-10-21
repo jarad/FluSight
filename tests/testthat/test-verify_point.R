1e974ac58106fec4632d2f5334371bde844dc9e1
Craig McGowan <mrx6@cdc.gov>
2016-10-21 11:04:47 -0400

    Add test for verify_probabilities
    Add minimal_entry to tests of verify_point

--- tests/testthat/test-verify_point.R
+++ tests/testthat/test-verify_point.R
@@ -1,6 +1,7 @@
 context("verify_point")
 
 test_that("Valid entry passes", {
+  expect_null(verify_point(minimal_entry))
   expect_null(verify_point(full_entry))
 })
 
@@ -8,12 +9,17 @@ test_that("Missing point predictions return warning", {
   rand_target <- sample(unique(minimal_entry$target), 1)
   rand_location <- sample(unique(full_entry$location), 1)
   
-  invalid_entry <- full_entry
-  invalid_entry$value[invalid_entry$location == rand_location & 
-                        invalid_entry$target == rand_target &
-                        invalid_entry$type == "Point"] <- NA
+  invalid_min <- minimal_entry
+  invalid_min$value[invalid_min$target == rand_target &
+                      invalid_min$type == "Point"] <- NA
   
-  expect_warning(verify_point(invalid_entry))
+  invalid_full <- full_entry
+  invalid_full$value[invalid_full$location == rand_location & 
+                       invalid_full$target == rand_target &
+                       invalid_full$type == "Point"] <- NA
+  
+  expect_warning(verify_point(invalid_min))
+  expect_warning(verify_point(invalid_full))
   
 })
 
@@ -21,11 +27,16 @@ test_that("Negative point prediction return error", {
   rand_target <- sample(unique(minimal_entry$target), 1)
   rand_location <- sample(unique(full_entry$location), 1)
   
-  invalid_entry <- full_entry
-  invalid_entry$value[invalid_entry$location == rand_location & 
-                        invalid_entry$target == rand_target &
-                        invalid_entry$type == "Point"] <- -1
+  invalid_min <- minimal_entry
+  invalid_min$value[invalid_min$target == rand_target &
+                      invalid_min$type == "Point"] <- -1
   
-  expect_error(verify_point(invalid_entry))
+  invalid_full <- full_entry
+  invalid_full$value[invalid_full$location == rand_location & 
+                       invalid_full$target == rand_target &
+                       invalid_full$type == "Point"] <- -1
+  
+  expect_error(verify_point(invalid_min))
+  expect_error(verify_point(invalid_full))
 
 })
---
+++ tests/testthat/test-verify_probabilities.R
@@ -0,0 +1,75 @@
+context("verify_probabilities")
+
+
+test_that("Valid entry passes", {
+  expect_null(verify_probabilities(minimal_entry))
+  expect_null(verify_probabilities(full_entry))
+})
+
+test_that("Missing probabilities throw errors", {
+  rand_target <- sample(unique(minimal_entry$target), 1)
+  rand_location <- sample(unique(full_entry$location), 1)
+  
+  invalid_min <- minimal_entry
+  invalid_min$value[invalid_min$target == rand_target &
+                      invalid_min$type == "Bin"] <- NA
+  invalid_full <- full_entry
+  invalid_full$value[invalid_full$location == rand_location &
+                       invalid_full$target == rand_target &
+                       invalid_full$type == "Bin"] <- NA
+  
+  expect_error(verify_probabilities(invalid_min))
+  expect_error(verify_probabilities(invalid_full))
+  
+})
+
+test_that("Negative probabilities throw errors", {
+  rand_target <- sample(unique(minimal_entry$target), 1)
+  rand_location <- sample(unique(full_entry$location), 1)
+  
+  invalid_min <- minimal_entry
+  invalid_min$value[invalid_min$target == rand_target &
+                      invalid_min$type == "Bin"] <- -0.1
+  invalid_full <- full_entry
+  invalid_full$value[invalid_full$location == rand_location &
+                       invalid_full$target == rand_target &
+                       invalid_full$type == "Bin"] <- -0.1
+  
+  expect_error(verify_probabilities(invalid_min))
+  expect_error(verify_probabilities(invalid_full))
+  
+})
+
+test_that("Probabilities summing to < 0.9 throw errors", {
+  rand_target <- sample(unique(minimal_entry$target), 1)
+  rand_location <- sample(unique(full_entry$location), 1)
+  
+  invalid_min <- minimal_entry
+  invalid_min$value[invalid_min$target == rand_target &
+                      invalid_min$type == "Bin"] <- 0.01
+  invalid_full <- full_entry
+  invalid_full$value[invalid_full$location == rand_location &
+                       invalid_full$target == rand_target &
+                       invalid_full$type == "Bin"] <- 0.01
+  
+  expect_error(verify_probabilities(invalid_min))
+  expect_error(verify_probabilities(invalid_full))
+  
+})
+
+test_that("Probabilities summing to > 1.1 throw errors", {
+  rand_target <- sample(unique(minimal_entry$target), 1)
+  rand_location <- sample(unique(full_entry$location), 1)
+  
+  invalid_min <- minimal_entry
+  invalid_min$value[invalid_min$target == rand_target &
+                      invalid_min$type == "Bin"] <- 0.1
+  invalid_full <- full_entry
+  invalid_full$value[invalid_full$location == rand_location &
+                       invalid_full$target == rand_target &
+                       invalid_full$type == "Bin"] <- 0.1
+  
+  expect_error(verify_probabilities(invalid_min))
+  expect_error(verify_probabilities(invalid_full))
+  
+})
