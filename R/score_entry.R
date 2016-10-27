984fc52171ec11b9624f3654ce8044a5abf6c47e
Craig McGowan <mrx6@cdc.gov>
2016-10-27 12:25:04 -0400

    Update score_entry to use forecast_week in scoring weekly targets

--- R/score_entry.R
+++ R/score_entry.R
@@ -10,14 +10,25 @@
 #' @return A data.frame of scores for each target
 score_entry <- function(entry, truth) {
 
-  entry %>%
-    filter(type == "Bin") %>%
-    right_join(truth, by=c("location", "target",
-                           "bin_start_incl")) %>%  # Will need to add forecast_wk here before bin_start_incl
-    group_by(location, target, forecast_wk) %>%
-    mutate(score = log(sum(value))) %>%
-    select(location, target, score, forecast_wk) %>%
-    unique()
+  seasonal <- entry %>%
+    filter(type == "Bin", target %in% c("Season onset", "Season peak week",
+                                        "Season peak percentage")) %>%
+    inner_join(truth, by=c("location", "target",
+                           "bin_start_incl")) %>%
+    select(-forecast_week.y,
+           forecast_week = forecast_week.x)
+  
+  weekly <- entry %>%
+    filter(type == "Bin", target %in% c("1 wk ahead", "2 wk ahead",
+                                        "3 wk ahead", "4 wk ahead")) %>%
+    inner_join(truth, by=c("location", "target",
+                           "bin_start_incl", "forecast_week"))           
+               
+  scores <- bind_rows(seasonal, weekly) %>%             
+              group_by(location, target, forecast_week) %>%
+              mutate(score = log(sum(value))) %>%
+              select(location, target, score, forecast_week) %>%
+              unique()
+  
+  return(scores)
 }
-
-
