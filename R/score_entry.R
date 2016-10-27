18e2abbd1dd29dab9166c4b4a8e6c2b943a9fdf2
Craig McGowan <mrx6@cdc.gov>
2016-10-27 13:25:15 -0400

    Added functionality for incomplete submissions

--- R/score_entry.R
+++ R/score_entry.R
@@ -1,34 +1,51 @@
 #' Scores an entry
 #'
 #' @param entry An entry data.frame with columns location, target,
-#' bin_start_incl, and value
+#' bin_start_incl, value, and forecast week
 #' @param truth A data.frame containing all true values with columns 
 #' location, target, forecast_wk, and bin_start_incl. If multiple bins are 
 #' considered correct for a given target, all correct bins must be included
 #' here.
+#' @seealso \code{\link{expand_truth}}
 #' @import dplyr
 #' @return A data.frame of scores for each target
+#' @examples 
+#' scores < - score(full_entry_week, truth_1516)
+#' 
 score_entry <- function(entry, truth) {
 
+  # Check for missing forecast week
+  if (!("forecast_week" %in% names(entry)))
+    stop("Forecast week needed in entry - 
+         use read_entry() with your submission CSV")
+  
   seasonal <- entry %>%
     filter(type == "Bin", target %in% c("Season onset", "Season peak week",
                                         "Season peak percentage")) %>%
-    inner_join(truth, by=c("location", "target",
+    right_join(truth, by=c("location", "target",
                            "bin_start_incl")) %>%
-    select(-forecast_week.y,
-           forecast_week = forecast_week.x)
+    filter(target %in% c("Season onset", "Season peak week",
+                         "Season peak percentage")) %>%
+    select(-forecast_week.x, -forecast_week.y) %>%
+    mutate(forecast_week = entry$forecast_week[1])
   
   weekly <- entry %>%
     filter(type == "Bin", target %in% c("1 wk ahead", "2 wk ahead",
                                         "3 wk ahead", "4 wk ahead")) %>%
-    inner_join(truth, by=c("location", "target",
-                           "bin_start_incl", "forecast_week"))           
+    right_join(truth, by=c("location", "target",
+                           "bin_start_incl", "forecast_week")) %>%
+    filter(target %in% c("1 wk ahead", "2 wk ahead",
+                         "3 wk ahead", "4 wk ahead"),
+           forecast_week == entry$forecast_week[1])
                
   scores <- bind_rows(seasonal, weekly) %>%             
               group_by(location, target, forecast_week) %>%
               mutate(score = log(sum(value))) %>%
               select(location, target, score, forecast_week) %>%
               unique()
+  
+  # If score < -10 or forecast is missing, set to -10
+  scores$score[scores$score < -10 | is.na(scores$score)] <- -10
   
   return(scores)
 }
