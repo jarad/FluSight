3489a92663b26b69246b28142d8558141b64b039
Craig McGowan <mrx6@cdc.gov>
2016-11-02 10:01:24 -0400

    Add details to package documentation

--- R/FluSight.R
+++ R/FluSight.R
@@ -1,6 +1,22 @@
 #' FluSight: a package for CDC Flu Competition functions
 #'
-#' something
+#' The FluSight package provides functions to assist with creating, verifying, 
+#' and scoring submissions to the CDC Influenza Forecasting Competition.
+#' 
+#' @section Creation functions:
+#' The functions \code{\link{generate_point_forecasts}}, 
+#' \code{\link{normalize_probs}}, and \code{\link{write_entry}} help create,
+#' format, and label submission files.
+#' 
+#' @section Verification functions:
+#' The functions \code{\link{verify_entry_file}} and \code{\link{verify_entry}}
+#' verify that a submission is in the correct format and can be scored.
+#' 
+#' @section Scoring functions:
+#' The functions \code{\link{create_truth}} and \code{\link{expand_truth}} 
+#' create the observed truth that submissions will be scored against. The
+#' function \code{\link{score_entry}} uses that observed truth to score an
+#' entry
 #'
 #' @docType package
 #' @name FluSight
