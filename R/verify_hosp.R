4b75dee4fc27c7a4a13918b2256dace09b34b5a1
Craig McGowan <mrx6@cdc.gov>
2017-09-07 12:23:06 -0400

    Functions to check validity of observed hospitalization values, along with tests and documentation

---
+++ R/verify_hosp.R
@@ -0,0 +1,42 @@
+#' Verify an observed hospitalizations file
+#'
+#' Verifies user-supplied FluSurv-NET hospitalization data to create
+#' observed truth
+#'
+#' @param observe_hosp A data.frame of observed FluSurv-NET values
+#' @return \code{NULL} or a descriptive error or warning message
+#' @export
+#' @examples
+#' verify_hosp(valid_observe_hosp)
+ 
+
+verify_hosp <- function(observe_hosp) {
+ 
+  # Verify column names
+  sub_names <- colnames(observe_hosp)
+  valid_names <- colnames(valid_observe_hosp)
+  
+  missing_names <- setdiff(valid_names, sub_names)
+  extra_names   <- setdiff(sub_names, valid_names)
+  
+  if (length(missing_names)>0)
+    stop("Missing these columns: ", paste(missing_names))
+  
+  if (length(extra_names)>0)
+    warning("These extra columns are ignored: ", paste(extra_names))
+  
+  
+  # Verify age groups
+  sub_agegrp <- unique(observe_hosp$age_grp)
+  valid_agegrp <- unique(valid_observe_hosp$age_grp)
+  
+  missing_names <- setdiff(valid_agegrp, sub_agegrp)
+  extra_names   <- setdiff(sub_agegrp, valid_agegrp)
+  
+  if (length(missing_names)>0)
+    warning("Missing these age groups: ", paste(missing_names), "; no targets
+            will be generated for these age groups")
+  
+  if (length(extra_names)>0)
+    stop("These age groups are invalid: ", paste(extra_names))
+}
---
+++ tests/testthat/test-verify_hosp.R
@@ -0,0 +1,30 @@
+context("verify_hosp")
+
+
+test_that("Valid entry is successful", {
+  expect_null(verify_hosp(valid_observe_hosp))
+})
+
+test_that("Missing columns throw errors", {
+  rand_col <- sample(names(valid_observe_hosp), 1)
+  tmp_hosp <- valid_observe_hosp[,-which(names(valid_observe_hosp) == "randcol")]
+  expect_error(verify_hosp(tmp_hosp))
+})
+
+test_that("Extra columns throw warnings", {
+  tmp_ILI <- valid_observe_hosp
+  tmp_ILI$extra <- NA
+  expect_warning(verify_hosp(tmp_ILI))
+})
+
+test_that("Missing age groups throw warnings", {
+  rand_age <- sample(unique(valid_observe_hosp$age_grp), 1)
+  tmp_ILI <- valid_observe_hosp[valid_observe_hosp$age_grp != rand_age, ]
+  expect_warning(verify_hosp(tmp_ILI))
+})
+
+test_that("Extra age group throw errors", {
+  tmp_ILI <- valid_observe_hosp
+  tmp_ILI$age_grp[1] <- "extra"
+  expect_error(verify_hosp(tmp_ILI))
+})
