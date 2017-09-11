052531fce589765c4a82de0939ab445f12e14fc5
Craig McGowan <mrx6@cdc.gov>
2017-09-11 16:01:32 -0400

    Fix file paths and streamline tests for verify_entry

--- tests/testthat/test-verify_entry.R
+++ tests/testthat/test-verify_entry.R
@@ -1,22 +1,34 @@
 context("verify_entry")
 
-valid_file_week  <- system.file("inst/extdata", "EW44_ValidTest_2016-11-07.csv", package="FluSight")
+valid_file_week <- system.file("extdata/EW44_ValidTest_2016-11-07.csv", package = "FluSight")
 valid_entry_week <- read_entry(valid_file_week)
-valid_file <- system.file("inst/extdata", "valid_test.csv", package="FluSight")
+valid_file <- system.file("/extdata", "valid_test.csv", package="FluSight")
 valid_entry <- read_entry(valid_file)
-valid_hosp_file <- system.file("inst/extdata", "EW44_ValidHospTest_2017-11-07.csv", package="FluSight")
+valid_hosp_file <- system.file("extdata", "EW48_ValidHospTest_2017-12-04.csv", package="FluSight")
 valid_hosp_entry <- read_entry(valid_hosp_file, challenge = "hospital")
+valid_state_file <- system.file("extdata", "EW44_ValidStateTest_2017-11-07.csv", package = "FluSight")
+valid_state_entry <- read_entry(valid_state_file, challenge = "state_ili")
+
 
 test_that("Valid entry passes", {
-	expect_true(verify_entry_file(valid_file ))
-	expect_true(verify_entry(     valid_entry))
+	expect_warning(verify_entry_file(valid_file ), 
+                "Missing forecast_week - verification will proceed but\n              forecast cannot be scored")
+	expect_warning(verify_entry(     valid_entry), 
+	               "Missing forecast_week - verification will proceed but\n              forecast cannot be scored")
 	expect_true(verify_entry_file(valid_file_week))
 	expect_true(verify_entry(valid_entry_week))
-	expect_true(verify_entry(      full_entry))
-	expect_true(verify_entry(  minimal_entry))
+	expect_warning(verify_entry(      full_entry), 
+	               "Missing forecast_week - verification will proceed but\n              forecast cannot be scored")
+	expect_warning(verify_entry(  minimal_entry), 
+	            "Missing forecast_week - verification will proceed but\n              forecast cannot be scored")
 	expect_true(verify_entry_file(valid_hosp_file, challenge = "hospital"))
 	expect_true(verify_entry(valid_hosp_entry, challenge = "hospital"))
-	expect_true(verify_entry(full_entry_hosp, challenge = "hospital"))
+	expect_warning(verify_entry(full_entry_hosp, challenge = "hospital"), 
+	               "Missing forecast_week - verification will proceed but\n              forecast cannot be scored")
+	expect_true(verify_entry_file(valid_state_file, challenge = "state_ili"))
+	expect_true(verify_entry(valid_state_entry, challenge = "state_ili"))
+	expect_warning(verify_entry(full_entry_state, challenge = "state_ili"), 
+	               "Missing forecast_week - verification will proceed but\n              forecast cannot be scored")
 })
 
 # test_that("Entry without forecast week generates warnings", {
@@ -26,93 +38,98 @@ test_that("Valid entry passes", {
 
 
 test_that("Return error when required column name doesn't exist", {
-  for (i in seq_along(names(valid_entry))) {
-  	invalid_entry <- valid_entry
-  	names(invalid_entry)[i] <- "invalidName"
-    expect_error(arrange_entry(invalid_entry))
-    expect_error(verify_entry( invalid_entry))
-  }
+  rand_column <- sample(names(valid_entry), 1)
+  invalid_entry <- valid_entry
+  names(invalid_entry)[names(invalid_entry) == rand_column] <- "invalidName"
+  expect_error(arrange_entry(invalid_entry))
+  expect_error(verify_entry( invalid_entry))
 })
 
 
 test_that("Return error when probabilities are missing", {
-	for (this_location in unique(valid_entry$location)) {
-		for (this_target in unique(valid_entry$target)) {
-			invalid_entry <- valid_entry
-			invalid_entry$value[invalid_entry$location == this_location &
-														invalid_entry$target == this_target &
-														invalid_entry$type == "Bin"] <- NA
-			expect_error(verify_probabilities(invalid_entry))
-			expect_error(verify_entry(        invalid_entry))
-		}
-	}
+	rand_location <- sample(unique(valid_entry_week$location), 1)
+	rand_target <- sample(unique(valid_entry_week$target), 1)
+
+	invalid_entry <- valid_entry_week
+	invalid_entry$value[invalid_entry$location == rand_location &
+												invalid_entry$target == rand_target &
+												invalid_entry$type == "Bin"] <- NA
+	
+	expect_error(verify_probabilities(invalid_entry))
+	expect_error(verify_entry(invalid_entry))
+
 })
 
 
 test_that("Return error when probabilities are negative", {
-	for (this_location in unique(valid_entry$location)) {
-		for (this_target in unique(valid_entry$target)) {
-			invalid_entry <- valid_entry
-			invalid_entry$value[invalid_entry$location == this_location &
-														invalid_entry$target == this_target &
-														invalid_entry$type == "Bin"] <- -0.5
-			expect_error(verify_probabilities(invalid_entry))
-			expect_error(verify_entry(        invalid_entry))
-		}
-	}
+  rand_location <- sample(unique(valid_entry_week$location), 1)
+  rand_target <- sample(unique(valid_entry_week$target), 1)
+  
+	invalid_entry <- valid_entry_week
+	invalid_entry$value[invalid_entry$location == rand_location &
+												invalid_entry$target == rand_target &
+												invalid_entry$type == "Bin"] <- -0.5
+	
+	expect_error(verify_probabilities(invalid_entry))
+	expect_error(verify_entry(invalid_entry))
+	
 })
 
 
 test_that("Return error when probabilities sum to less than 0.9", {
-	for (this_location in unique(valid_entry$location)) {
-		for (this_target in unique(valid_entry$target)) {
-			invalid_entry <- valid_entry
-			invalid_entry$value[invalid_entry$location == this_location &
-														invalid_entry$target == this_target &
-														invalid_entry$type == "Bin"] <- 0.01
-			expect_error(verify_probabilities(invalid_entry))
-			expect_error(verify_entry(        invalid_entry))
-		}
-	}
+  rand_location <- sample(unique(valid_entry_week$location), 1)
+  rand_target <- sample(unique(valid_entry_week$target), 1)
+  
+	invalid_entry <- valid_entry_week
+	invalid_entry$value[invalid_entry$location == rand_location &
+												invalid_entry$target == rand_target &
+												invalid_entry$type == "Bin"] <- 0.01
+	
+	expect_error(verify_probabilities(invalid_entry))
+	expect_error(verify_entry(invalid_entry))
+	
+	
 })
 
 
 test_that("Return error when probabilities sum to more than 1.1", {
-	for (this_location in unique(valid_entry$location)) {
-		for (this_target in unique(valid_entry$target)) {
-			invalid_entry <- valid_entry
-			invalid_entry$value[invalid_entry$location == this_location &
-														invalid_entry$target == this_target &
-														invalid_entry$type == "Bin"] <- 0.1
-			expect_error(verify_probabilities(invalid_entry))
-			expect_error(verify_entry(        invalid_entry))
-		}
-	}
+  rand_location <- sample(unique(valid_entry_week$location), 1)
+  rand_target <- sample(unique(valid_entry_week$target), 1)
+  
+	invalid_entry <- valid_entry_week
+	invalid_entry$value[invalid_entry$location == rand_location &
+												invalid_entry$target == rand_target &
+												invalid_entry$type == "Bin"] <- 0.1
+	
+	expect_error(verify_probabilities(invalid_entry))
+	expect_error(verify_entry(invalid_entry))
+	
 })
 
 test_that("Return warning when point forecast is missing", {
-	for (this_location in unique(valid_entry$location)) {
-		for (this_target in unique(valid_entry$target)) {
-			invalid_entry <- valid_entry
-			invalid_entry$value[invalid_entry$location == this_location &
-														invalid_entry$target == this_target &
-														invalid_entry$type == "Point"] <- NA
-			expect_warning(verify_point(invalid_entry))
-			expect_warning(verify_entry(invalid_entry))
-		}
-	}
+  rand_location <- sample(unique(valid_entry_week$location), 1)
+  rand_target <- sample(unique(valid_entry_week$target), 1)
+			
+  invalid_entry <- valid_entry_week
+	invalid_entry$value[invalid_entry$location == rand_location &
+												invalid_entry$target == rand_target &
+												invalid_entry$type == "Point"] <- NA
+	
+	expect_warning(verify_point(invalid_entry))
+	expect_warning(verify_entry(invalid_entry))
+	
 })
 
 test_that("Return error when point forecast is negative", {
-	for (this_location in unique(valid_entry$location)) {
-		for (this_target in unique(valid_entry$target)) {
-			invalid_entry <- valid_entry
-			invalid_entry$value[invalid_entry$location == this_location &
-														invalid_entry$target == this_target &
-														invalid_entry$type == "Point"] <- -1
-			expect_error(verify_point(invalid_entry))
-			expect_error(verify_entry(invalid_entry))
-		}
-	}
+  rand_location <- sample(unique(valid_entry_week$location), 1)
+  rand_target <- sample(unique(valid_entry_week$target), 1)
+			
+  invalid_entry <- valid_entry_week
+	invalid_entry$value[invalid_entry$location == rand_location &
+												invalid_entry$target == rand_target &
+												invalid_entry$type == "Point"] <- -1
+	expect_error(verify_point(invalid_entry))
+	expect_error(verify_entry(invalid_entry))
+	
 })
 
